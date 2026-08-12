9af8f6b506ce1ab2aef296c116a2a3ed1b95c564
ashish sood <ashishsood1998@gmail.com>
2026-08-12 12:54:47 -0500

    Db context file created DAL and basic setup.

---
+++ EventHub.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventHub.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventHub.DAL.Data;
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+
+    public DbSet<Event> Events { get; set; }
+
+    public DbSet<Registration> Registrations { get; set; }
+
+
+ }
