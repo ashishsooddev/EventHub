c626042872268de15e3e20236d5c53bdf8fc3364
ashish sood <ashishsood1998@gmail.com>
2026-08-12 12:23:23 -0500

    models created for category

--- EventHub.Models/Category.cs
+++ EventHub.Models/Category.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EventHub.Models
+namespace EventHub.Models;
+
+public class Category
 {
-    internal class Category
-    {
-    }
+    public int CategoryId { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public ICollection<Event> Events { get; set; } = new List<Event>();
 }
