b14642bdbbbf50e3393722efd1846ce479750932
ashish sood <ashishsood1998@gmail.com>
2026-08-12 12:24:59 -0500

    models for event file created

--- EventHub.Models/Event.cs
+++ EventHub.Models/Event.cs
@@ -4,9 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EventHub.Models
+namespace EventHub.Models;
+
+public class Event
 {
-    internal class Event
-    {
-    }
+    public int EventId { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public DateTime EventDate { get; set; }
+
+    public string Location { get; set; } = string.Empty;
+
+    public int Capacity { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public Category? Category { get; set; }
+
+    public ICollection<Registration> Registrations { get; set; } = new List<Registration>();
 }
