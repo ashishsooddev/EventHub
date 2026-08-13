a242c9df17becf99fb98e5c99b1dc9b841d1d994
ashish sood <ashishsood1998@gmail.com>
2026-08-12 22:56:01 -0500

    update and Delete tasks added, Issues fixed.

--- EventHub.BLL/Services/CategoryService.cs
+++ EventHub.BLL/Services/CategoryService.cs
@@ -38,4 +38,22 @@ public class CategoryService
         await _context.SaveChangesAsync();
     }
 
+    public async Task UpdateAsync(Category category)
+    {
+        _context.Categories.Update(category);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var category = await _context.Categories
+            .FindAsync(id);
+
+        if (category != null)
+        {
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+        }
+    }
+
 }
