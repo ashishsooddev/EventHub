66e1fa60fc0b9dab6829204abf4c476cd03316e5
ashish sood <ashishsood1998@gmail.com>
2026-08-13 00:43:29 -0500

    create and edit tasks added and issues fixed.

--- EventHub/Controllers/CategoriesController.cs
+++ EventHub/Controllers/CategoriesController.cs
@@ -33,5 +33,59 @@ namespace EventHub.Controllers
             return View(category);
         }
 
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            await _categoryService.CreateAsync(category);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Edit(int id, Category category)
+        {
+            if (id != category.CategoryId)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            await _categoryService.UpdateAsync(category);
+            return RedirectToAction(nameof(Index));
+        }
+
+
     }
 }
