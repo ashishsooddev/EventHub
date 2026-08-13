56a2205309696be525eab0dd02b5dc2b0fd7c416
ashish sood <ashishsood1998@gmail.com>
2026-08-12 22:20:18 -0500

    Database created migration done

--- EventHub/Data/ApplicationDbContext.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<EventHub.Data.ApplicationUser>(options)
-{
-}
