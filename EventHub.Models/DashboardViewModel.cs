ad9d2ec84c972aafde86472e0d9540b9685172c5
ashish sood <ashishsood1998@gmail.com>
2026-08-13 09:45:30 -0500

    Dashboard controller added

---
+++ EventHub.Models/DashboardViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHub.Models;
+
+public class DashboardViewModel
+{
+    public int TotalEvents { get; set; }
+
+    public int TotalCategories { get; set; }
+
+    public int TotalRegistrations { get; set; }
+
+    public List<Event> UpcomingEvents { get; set; } = new List<Event>();
+}
