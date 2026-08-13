cf0465842763cb7b297e51fbb8dc37967216e6e9
ashish sood <ashishsood1998@gmail.com>
2026-08-12 23:01:22 -0500

    Registration service task added.

--- EventHub.BLL/Services/RegistrationService.cs
+++ EventHub.BLL/Services/RegistrationService.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EventHub.DAL.Data;
+using EventHub.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace EventHub.BLL.Services
+namespace EventHub.BLL.Services;
+
+public class RegistrationService
 {
-    internal class RegistrationService
+    private readonly ApplicationDbContext _context;
+
+    public RegistrationService(ApplicationDbContext context)
     {
+        _context = context;
     }
+
+    public async Task<List<Registration>> GetAllAsync()
+    {
+        return await _context.Registrations
+            .Include(r => r.Event)
+            .ToListAsync();
+    }
+
+    public async Task<Registration?> GetByIdAsync(int id)
+    {
+        return await _context.Registrations
+            .Include(r => r.Event)
+            .FirstOrDefaultAsync(r => r.RegistrationId == id);
+    }
+
 }
