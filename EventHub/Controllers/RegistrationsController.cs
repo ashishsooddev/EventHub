db6eb6e6d47aa896592ac120d85095c38919b349
ashish sood <ashishsood1998@gmail.com>
2026-08-13 01:01:56 -0500

    Details and create functionality added

--- EventHub/Controllers/RegistrationsController.cs
+++ EventHub/Controllers/RegistrationsController.cs
@@ -12,5 +12,41 @@ namespace EventHub.Controllers
         {
             _registrationService = registrationService;
         }
+        public async Task<IActionResult> Index()
+        {
+            var registrations = await _registrationService.GetAllAsync();
+            return View(registrations);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var registration = await _registrationService.GetByIdAsync(id);
+
+            if (registration == null)
+            {
+                return NotFound();
+            }
+            return View(registration);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Registration registration)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registration);
+            }
+
+            registration.RegistrationDate = DateTime.Now;
+
+            await _registrationService.CreateAsync(registration);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
