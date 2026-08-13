9b64b37211157cfc12a4e18c31d10326d9169326
ashish sood <ashishsood1998@gmail.com>
2026-08-12 22:30:05 -0500

    Database updated issues fixed.

---
+++ EventHub.DAL/Migrations/20260813032913_AddEventCategory.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventHub.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEventCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Categories_CategoryId",
+                table: "Events");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Registrations_AspNetUsers_UserId",
+                table: "Registrations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Registrations_UserId_EventId",
+                table: "Registrations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_CategoryId",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Events");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Events",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1200)",
+                oldMaxLength: 1200);
+
+            migrationBuilder.CreateTable(
+                name: "EventCategories",
+                columns: table => new
+                {
+                    CategoriesCategoryId = table.Column<int>(type: "int", nullable: false),
+                    EventsEventId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventCategories", x => new { x.CategoriesCategoryId, x.EventsEventId });
+                    table.ForeignKey(
+                        name: "FK_EventCategories_Categories_CategoriesCategoryId",
+                        column: x => x.CategoriesCategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EventCategories_Events_EventsEventId",
+                        column: x => x.EventsEventId,
+                        principalTable: "Events",
+                        principalColumn: "EventId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventCategories_EventsEventId",
+                table: "EventCategories",
+                column: "EventsEventId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventCategories");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Events",
+                type: "nvarchar(1200)",
+                maxLength: 1200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registrations_UserId_EventId",
+                table: "Registrations",
+                columns: new[] { "UserId", "EventId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_CategoryId",
+                table: "Events",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Categories_CategoryId",
+                table: "Events",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Registrations_AspNetUsers_UserId",
+                table: "Registrations",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
