0eb8ad6e36d85530acd05ae56f4a63dfc3a436de
ashish sood <ashishsood1998@gmail.com>
2026-08-12 22:35:38 -0500

    tasks added in event service

--- EventHub.BLL/Services/EventService.cs
+++ EventHub.BLL/Services/EventService.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EventHub.DAL.Data;
+using EventHub.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace EventHub.BLL.Services
+namespace EventHub.BLL.Services;
+
+public class EventService
 {
-    internal class EventService
+    private readonly ApplicationDbContext _context;
+
+    public EventService(ApplicationDbContext context)
     {
+        _context = context;
     }
+
+    public async Task<List<Event>> GetAllAsync()
+    {
+        return await _context.Events
+            .Include(e => e.Categories)
+            .Include(e => e.Registrations)
+            .ToListAsync();
+    }
+
+    public async Task<Event?> GetByIdAsync(int id)
+    {
+        return await _context.Events
+            .Include(e => e.Categories)
+            .Include(e => e.Registrations)
+            .FirstOrDefaultAsync(e => e.EventId == id);
+    }
+
 }
