a6de0bd3febee8d9267313a2ba09fbf5dd02b158
ashish sood <ashishsood1998@gmail.com>
2026-08-12 23:52:06 -0500

    Controllers created and added

---
+++ EventHub/Controllers/CategoriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventHub.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EventHub/Controllers/RegistrationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventHub.Controllers
+{
+    public class RegistrationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
