a2c3f4ab7c105abd3144c357b23d65f7ddeef158
ashish sood <ashishsood1998@gmail.com>
2026-08-13 00:53:20 -0500

    Issues fixed

--- EventHub/Controllers/RegistrationsController.cs
+++ EventHub/Controllers/RegistrationsController.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using EventHub.BLL.Services;
+using EventHub.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace EventHub.Controllers
 {
     public class RegistrationsController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+
     }
 }
