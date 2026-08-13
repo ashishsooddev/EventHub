9847ef93ca23ebd544070fba58ae246f5ccc3375
ashish sood <ashishsood1998@gmail.com>
2026-08-13 09:46:22 -0500

    Home controller updated

--- EventHub/Controllers/HomeController.cs
+++ EventHub/Controllers/HomeController.cs
@@ -1,32 +1,40 @@
+using EventHub.DAL.Data;
 using EventHub.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 
 namespace EventHub.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var dashboard = new DashboardViewModel
+            {
+                TotalEvents = await _context.Events.CountAsync(),
+                TotalCategories = await _context.Categories.CountAsync(),
+                TotalRegistrations = await _context.Registrations.CountAsync(),
+
+                UpcomingEvents = await _context.Events
+                    .Where(e => e.EventDate >= DateTime.Now)
+                    .OrderBy(e => e.EventDate)
+                    .Take(5)
+                    .ToListAsync()
+            };
+
+            return View(dashboard);
         }
 
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
